29bc4ede36fec6a99d612ffac433612b8ea5b88f
dlyanghao <369000246@qq.com>
2016-05-19 16:34:57 +0800

    版本1.0

---
+++ environment/src/com/yanghao/control/AnnnotationController.java
@@ -0,0 +1,13 @@
+package com.yanghao.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AnnnotationController {
+	@RequestMapping("login.do")
+	public String login(){
+		return "login";
+	}
+
+}
